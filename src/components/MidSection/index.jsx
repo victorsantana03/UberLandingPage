093ae2753b1db5f806902ada74f3800d7cdb65bf
victorsantana03 <viclucigei.123@gmail.com>
2024-10-08 15:25:25 -0300

    MidSection finalizada

--- src/components/MidSection/index.jsx
+++ src/components/MidSection/index.jsx
@@ -6,39 +6,45 @@ import trafficImage from "../../assets/images/traffic.png"
 
 export function MidSection() {
     return (
-        <section className='bg-blue-500 absolute h-screen w-full overflow-x-hidden'>
+        <section className='absolute h-screen w-full overflow-x-hidden'>
 
             <div className='h-24 bg-cover sm:h-40 xl:h-72 bg-center w-screen' style={{ backgroundImage: `url(${backgroundImage})` }}>
                 <div className="w-1/2 flex flex-col gap-6 text-left ml-52 pt-20">
                     <h1 className="text-5xl font-bold">Uber For Business</h1>
                     <p className="font-medium text-lg">Trasnform the way your company moves and feeds its people.</p>
                     <button className="bg-black flex items-center justify-center gap-2 text-white text-sm font-bold w-2/3 sm:w-44 rounded py-2 sm:py-4 sm:px-7 hover:bg-slate-900">
-                        See How 
-                        <GoArrowRight className="text-white text-xl"/>
+                        See How
+                        <GoArrowRight className="text-white text-xl" />
                     </button>
                 </div>
             </div>
 
-            <div className="flex flex-col items-center justify-between">
-                <h1>Focus on safety, Wherever you go</h1>
-                <div className="flex ">
-                    <div className="w-2/5">
-                        <img src={driveImage}/>
-                        <h2>Our Commitment to your safety</h2>
-                        <p>With every safety feature and every standard in our Community Guidelines,
-                        we're committed to helping to create a safe environment for our users.</p>
-                        <div>
-                            <p>Read About Our Community Guidelines</p>
-                            <p>See all safety features</p>
+            <div className="mx-40">
+                <h1 className="pt-10 pb-5 text-2xl font-bold">Focus on safety, Wherever you go</h1>
+                <div className="flex justify-between">
+                    <div className="w-2/5  bg-white" >
+                        <div className="h-96 bg-center rounded-xl" style={{ backgroundImage: `url(${driveImage})` }}></div>
+                        <div className="pt-8 flex flex-col gap-5">
+                            <h2 className="text-xl font-medium">Our Commitment to your safety</h2>
+                            <p className="text-gray-700">With every safety feature and every standard in our Community Guidelines,
+                                we're committed to helping to create a safe environment for our users.</p>
+                            <div className="flex gap-9">
+                                <p className="border-b-2 border-black border-dotted">Read About Our Community Guidelines</p>
+                                <p className="border-b-2 border-black border-dotted">See all safety features</p>
+                            </div>
                         </div>
                     </div>
 
-                    <div className="w-2/5">
-                        <img src={trafficImage}/>
-                        <h2>Setting 10,000+ cities in motion</h2>
-                        <p>The app is available in thousands of cities worldwide, so you can request a
-                        ride even when you’re far from home.</p>
-                        <p>View All Cities</p>
+                    <div className="w-2/5  bg-white">
+                        <div className="h-96 bg-center rounded-xl" style={{ backgroundImage: `url(${trafficImage})` }}></div>
+                        <div className="pt-8 flex flex-col gap-5">
+                            <h2 className="text-xl font-medium">Setting 10,000+ cities in motion</h2>
+                            <p className="text-gray-700">The app is available in thousands of cities worldwide, so you can request a
+                                ride even when you’re far from home.</p>
+                            <div className="flex">
+                                <p className="border-b-2 border-black border-dotted">View All Cities</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
